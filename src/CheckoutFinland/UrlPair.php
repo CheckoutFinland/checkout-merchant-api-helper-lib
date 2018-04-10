98c04f40c3c958fd65743ffbdac8406cbfab69ce
Tommi Reinikainen <tommi.reinikainen@checkout.fi>
2018-04-10 11:46:37 +0300

    urlpair fields

--- src/CheckoutFinland/UrlPair.php
+++ src/CheckoutFinland/UrlPair.php
@@ -3,6 +3,17 @@ namespace CheckoutFinland;
 
 class RedirectUrl
 {
+    private $success;
+    private $cancel;
+
+    public function __construct(
+        string $success,
+        string $cancel
+    ) {
+        $this->success;
+        $this->cancel;
+    }
+
     public function expose(): array
     {
         return get_object_vars($this);
