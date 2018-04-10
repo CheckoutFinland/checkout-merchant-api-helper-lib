4a9192b2c7c8514e1e7c53fc52f11a4747528751
Tommi Reinikainen <tommi.reinikainen@checkout.fi>
2018-04-10 11:48:33 +0300

    customer fields

--- src/CheckoutFinland/Customer.php
+++ src/CheckoutFinland/Customer.php
@@ -3,6 +3,26 @@ namespace CheckoutFinland;
 
 class Customer
 {
+    private $email;
+    private $firstName;
+    private $lastName;
+    private $phone;
+    private $vatId;
+
+    public function __construct(
+        string $email,
+        string $firstName,
+        string $lastName,
+        string $phone,
+        string $vatId
+    ) {
+        $this->email;
+        $this->firstName;
+        $this->lastName;
+        $this->phone;
+        $this->vatId;
+    }
+
     public function expose(): array
     {
         return get_object_vars($this);
