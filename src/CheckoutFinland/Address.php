fcc20875af133bdbdee5f145ef2a22ae66730d6b
Tommi Reinikainen <tommi.reinikainen@checkout.fi>
2018-04-10 11:45:05 +0300

    added fields to address

--- src/CheckoutFinland/Address.php
+++ src/CheckoutFinland/Address.php
@@ -3,6 +3,26 @@ namespace CheckoutFinland;
 
 class Address
 {
+    private $streetAddress;
+    private $postalCode;
+    private $city;
+    private $county;
+    private $country;
+
+    public function __construct(
+        string $streetAddress,
+        string $postalCode,
+        string $city,
+        string $county,
+        string $country
+    ) {
+        $this->streetAddress;
+        $this->postalCode;
+        $this->city;
+        $this->county;
+        $this->country;
+    }
+
     public function expose(): array
     {
         return get_object_vars($this);
